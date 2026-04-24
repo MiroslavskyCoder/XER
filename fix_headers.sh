0fddea9fc2052dc8e41533c6c995e67add6b35da
MiroslavskyCoder <programmer_js@vk.com>
2026-04-24 17:54:30 +0300

    Remove unused scripts for creating Skia modules and fixing headers

--- fix_headers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIR="src/wrapper/skia"
-
-sed -i '1s/^/#include <cstdint>\n/' "$DIR/skia_engine_bridge_image_filter.h"
-sed -i '1s/^/#include <vector>\n/' "$DIR/skia_engine_bridge_shader.h"
-
